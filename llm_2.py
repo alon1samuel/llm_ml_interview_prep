75e66625e1ee10e422bcda42f1d8106253cab1f1
Alon Samuel <alon1samuel@gmail.com>
2024-11-15 13:09:11 +0000

    llm_2 rag implementation

--- llm_2.py
+++ llm_2.py
@@ -8,6 +8,13 @@ Use Faiss to enhance semantic search by integrating with an LLM for context-base
 """
 
 
+import getpass
+import os
+
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")
+
+
 # Implement RAG (Retrieval-Augmented Generation) for document retrieval and answering.
 
 from langchain_community.document_loaders import WebBaseLoader
@@ -18,3 +25,74 @@ data = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 all_splits = text_splitter.split_documents(data)
+
+from langchain_chroma import Chroma
+
+from langchain_openai import OpenAIEmbeddings
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    # With the `text-embedding-3` class
+    # of models, you can specify the size
+    # of the embeddings you want returned.
+    # dimensions=1024
+)
+
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=embeddings)
+
+question = "What are the approaches to Task Decomposition?"
+docs = vectorstore.similarity_search(question)
+print(len(docs))
+
+
+print()
+
+
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI()
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+
+# Convert loaded documents into strings by concatenating their content
+# and ignoring metadata
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+question = "What are the approaches to Task Decomposition?"
+
+docs = vectorstore.similarity_search(question)
+
+
+
+from langchain_core.runnables import RunnablePassthrough
+
+RAG_TEMPLATE = """
+You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+
+<context>
+{context}
+</context>
+
+Answer the following question:
+
+{question}"""
+
+rag_prompt = ChatPromptTemplate.from_template(RAG_TEMPLATE)
+
+chain = (
+    RunnablePassthrough.assign(context=lambda input: format_docs(input["context"]))
+    | rag_prompt
+    | model
+    | StrOutputParser()
+)
+
+question = "What are the approaches to Task Decomposition?"
+
+docs = vectorstore.similarity_search(question)
+
+# Run
+chain.invoke({"context": docs, "question": question})
